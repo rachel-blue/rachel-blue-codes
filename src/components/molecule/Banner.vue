2e1965406f9aec7ba90a70c660a2dfb70096429c
Rachel <rachel17.22@gmail.com>
2021-04-22 14:26:49 -0600

    banner component now has icons and profile picture

--- src/components/molecule/Banner.vue
+++ src/components/molecule/Banner.vue
@@ -1,14 +1,49 @@
 <template>
-  <div class="bg-warning">my face will go here</div>
+  <div>
+    <img
+      class="m-banner_pic"
+      alt="Ray Blue"
+      src="../../assets/RayBlue.jpg"
+    />
+
+    <div class="m-banner_icons">
+      <div><font-awesome-icon :icon="github" class="fa-5x"/></div>
+      <div><font-awesome-icon :icon="linkedin" class="fa-5x"/></div>
+      <div><font-awesome-icon :icon="twitter" class="fa-5x"/></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithubSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
 export default {
   name: 'Banner',
-  props: {}
+  props: {},
+  data () {
+    return {
+      github: faGithubSquare,
+      linkedin: faLinkedin,
+      twitter: faTwitterSquare,
+    }
+  },
+
+  components: {
+    FontAwesomeIcon
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.m-banner {
+  &_pic {
+    width: 100%;
+    height: auto;
+  }
 
+  &_icons {
+    padding-left: 60px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGithubSquare)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
